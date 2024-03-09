079ca08d295563a1294c263c366ce838fe6a09ba
Ivan Xu <info@chivalrystudio.com>
2024-03-08 18:38:29 -0800

    s

--- apps/cpu_dispatcher.cu
+++ apps/cpu_dispatcher.cu
@@ -16,8 +16,8 @@ void cpu::v2::dispatch_RadixSort(const int n_threads, const Pipe& pipe) {
 
 void cpu::v2::dispatch_RemoveDuplicates([[maybe_unused]] int n_threads,
                                         Pipe& pipe) {
-  std::copy(pipe.sort.u_sort,
-            pipe.sort.u_sort + pipe.getInputSize(),
+  std::copy_n(pipe.sort.u_sort,
+            pipe.getInputSize(),
             pipe.unique.u_keys_out);
 
   // const auto it = std::unique(pipe.unique.begin(),
